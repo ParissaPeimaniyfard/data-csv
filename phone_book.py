0cd6c9f1619158449af7a7922aecd6ab38292b44
Parissa <zahra.pmf@gmail.com>
2023-10-23 23:20:04 +0200

    Read CSV file module DictReader object

--- phone_book.py
+++ phone_book.py
@@ -1,10 +1,19 @@
+#import csv
+
+#with open('data/phone_book.csv', mode='r') as csv_file:
+    #csv_reader = csv.reader(csv_file)
+    #line_count = 0
+    #for row in csv_reader:
+        #if line_count==0:
+            #line_count += 1
+            #continue
+        #print(f"{row[1]}: {row[2]}")
+
+
 import csv
 
 with open('data/phone_book.csv', mode='r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    line_count = 0
+    csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
-        if line_count==0:
-            line_count += 1
-            continue
-        print(f"{row[1]}: {row[2]}")
+        #print (type(row))
+        print(row["first_name"], row["last_name"], row["phone_number"])
